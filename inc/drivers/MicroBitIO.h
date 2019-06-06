83dbc3098f47926beafe095246f162593c292550
baradaran <amirhossein.baradaran@gmail.com>
2019-06-06 17:12:01 +0200

    added more pins

--- inc/drivers/MicroBitIO.h
+++ inc/drivers/MicroBitIO.h
@@ -42,25 +42,27 @@ class MicroBitIO
 
 	MicroBitPin			 pin[0];
 	MicroBitPin          P0;
-    MicroBitPin          P1;
-    MicroBitPin          P2;
-    MicroBitPin          P3;
-    MicroBitPin          P4;
-    MicroBitPin          P5;
-    MicroBitPin          P6;
-    MicroBitPin          P7;
-    MicroBitPin          P8;
-    MicroBitPin          P9;
-    MicroBitPin          P10;
-    MicroBitPin          P11;
-    MicroBitPin          P12;
-    MicroBitPin          P13;
-    MicroBitPin          P14;
-    MicroBitPin          P15;
-    MicroBitPin          P16;
-    MicroBitPin          P19;
-    MicroBitPin          P20;
-    MicroBitPin          P21;
+  MicroBitPin          P1;
+  MicroBitPin          P2;
+  MicroBitPin          P3;
+  MicroBitPin          P4;
+  MicroBitPin          P5;
+  MicroBitPin          P6;
+  MicroBitPin          P7;
+  MicroBitPin          P8;
+  MicroBitPin          P9;
+  MicroBitPin          P10;
+  MicroBitPin          P11;
+  MicroBitPin          P12;
+  MicroBitPin          P13;
+  MicroBitPin          P14;
+  MicroBitPin          P15;
+  MicroBitPin          P16;
+  MicroBitPin          P17;
+  MicroBitPin          P18;
+  MicroBitPin          P19;
+  MicroBitPin          P20;
+  MicroBitPin          P21;
 
     /**
       * Constructor.
@@ -75,7 +77,7 @@ class MicroBitIO
                int ID_P6, int ID_P7, int ID_P8,
                int ID_P9, int ID_P10,int ID_P11,
                int ID_P12,int ID_P13,int ID_P14,
-               int ID_P15,int ID_P16,int ID_P19,
+               int ID_P15,int ID_P16,int ID_P17, int ID_p18, int ID_P19,
                int ID_P20,int ID_P21);
 };
 
